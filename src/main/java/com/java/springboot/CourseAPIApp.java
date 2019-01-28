a6fd9c1873dbdc360c713f1efbf2df7a92914f49
punitsingh2 <46846628+punitsingh2@users.noreply.github.com>
2019-01-28 23:14:38 +0530

    added SpringBootServletInitializer

--- src/main/java/com/java/springboot/CourseAPIApp.java
+++ src/main/java/com/java/springboot/CourseAPIApp.java
@@ -2,9 +2,12 @@ package com.java.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CourseAPIApp {
+@ComponentScan(basePackages = "com.java.springboot")
+public class CourseAPIApp extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseAPIApp.class, args);
