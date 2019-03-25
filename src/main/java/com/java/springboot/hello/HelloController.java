f69487875e9c8ea2ef5248056f747c29989fde1b
punitsingh2 <46846628+punitsingh2@users.noreply.github.com>
2019-03-25 22:38:32 +0530

    one method added

--- src/main/java/com/java/springboot/hello/HelloController.java
+++ src/main/java/com/java/springboot/hello/HelloController.java
@@ -1,5 +1,9 @@
 package com.java.springboot.hello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,30 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String sayHi() {
 		return "Hi";
+	}
+	
+	@RequestMapping("/exec")
+	public String call() {
+		String[] cmd = {"sh","test.sh"};
+		String result = null;
+		 ProcessBuilder pb = new ProcessBuilder(cmd); 
+		 try
+		 {
+		 Process process = pb.start();
+		 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		 StringBuilder builder = new StringBuilder();
+		 String line = null;
+		 while ( (line = reader.readLine()) != null) {
+		 builder.append(line);
+		 }
+		  result = builder.toString();
+		 System.out.print(result);
+		 System.out.println("end of script execution");
+		 }
+		 catch (IOException e)
+		 { System.out.print("error");
+		 e.printStackTrace();
+		 }
+		 return result;
 	}
 }
